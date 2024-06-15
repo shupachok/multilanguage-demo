b16f3c0527a9450474852434f17d9022f6da524b
SUPACHOK\shupa <shupachok@gmail.com>
2024-06-15 07:33:25 +0700

    [add] initial project

---
+++ src/main/java/com/sp/multilanguagedemo/MultilanguageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sp.multilanguagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilanguageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultilanguageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/multilanguagedemo/MultilanguageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.multilanguagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultilanguageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
